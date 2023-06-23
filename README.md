fc16093d40359d40017c5d71d559a71a1cfabed8
keristo <casetiadharma@gmail.com>
2023-06-23 20:12:51 +0800

    update mock data to have most frequent items

---
+++ README.md
@@ -0,0 +1,1 @@
+Mostly from prompting in ChatGPT to test out its capabilities :)
--- gen_data.py
+++ gen_data.py
@@ -11,6 +11,35 @@ csv_file_path = "mock_data.csv"
 # Define the number of rows
 num_rows = 3000
 
+# Define the top item IDs for each flight number
+flight_top_items = {
+    "100": [1, 2, 3, 5],   # Flight 100 has top items with item IDs 1, 2, 3, 5
+    "101": [1, 4, 5, 6],   # Flight 101 has top items with item IDs 1, 4, 5, 6
+    "102": [2, 7, 8, 9],   # Flight 102 has top items with item IDs 2, 7, 8, 9
+    "103": [2, 5, 10],     # Flight 103 has top items with item IDs 2, 5, 10
+    "104": [3, 6, 8],      # Flight 104 has top items with item IDs 3, 6, 8
+    "105": [1, 4, 7],      # Flight 105 has top items with item IDs 1, 4, 7
+    "106": [9, 10],        # Flight 106 has top items with item IDs 9, 10
+    "107": [2, 3],         # Flight 107 has top items with item IDs 2, 3
+    "108": [4, 5, 8, 9],   # Flight 108 has top items with item IDs 4, 5, 8, 9
+    "109": [6, 7],         # Flight 109 has top items with item IDs 6, 7
+    "110": [8, 9, 10]      # Flight 110 has top items with item IDs 8, 9, 10
+}
+
+# Define the item lookup with item name, price, and weight
+item_lookup = {
+    1: ("Briyani", 0.5, 25.0),
+    2: ("Nasi Lemak", 0.7, 15.0),
+    3: ("Sandwich", 0.3, 12.0),
+    4: ("Sushi", 0.2, 30.0),
+    5: ("Dumplings", 0.4, 20.0),
+    6: ("Coke", 0.1, 5.0),
+    7: ("Beer", 0.3, 8.0),
+    8: ("Plane Miniature", 0.8, 50.0),
+    9: ("Perfume", 0.2, 80.0),
+    10: ("Hoodie", 1.5, 70.0)
+}
+
 # Generate random sample data
 data = []
 flight_departure_times = {}
@@ -25,26 +54,18 @@ for i in range(num_rows):
 
     departure_time = random.choice(flight_departure_times[flight_number])
     order_id = i + 1  # Assign order ID as the index value
-    item_id = random.randint(1, 10)
 
-    item_lookup = {
-        1: ("Briyani", 0.5),
-        2: ("Nasi Lemak", 0.7),
-        3: ("Sandwich", 0.3),
-        4: ("Sushi", 0.2),
-        5: ("Dumplings", 0.4),
-        6: ("Coke", 0.1),
-        7: ("Beer", 0.3),
-        8: ("Plane Miniature", 0.8),
-        9: ("Perfume", 0.2),
-        10: ("Hoodie", 1.5)
-    }
+    if flight_number in flight_top_items and bool(random.getrandbits(1)):
+        # Flight has top items, randomly select majority item ID from top items
+        item_id = random.choice(flight_top_items[flight_number])
+    else:
+        # Flight does not have top items, generate random item ID
+        item_id = random.randint(1, 10)
 
-    item_name, item_weight = item_lookup.get(item_id, ("Unknown", 0.0))
+    item_name, item_weight, item_price = item_lookup[item_id]
 
     user_id = random.randint(1, 100)
     seat_number = random.choice(["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"])
-    item_price = round(random.uniform(10, 100), 2)
     quantity = random.randint(1, 5)
     status = random.choice(["Out of Stock", "Success"])
     order_time = departure_time + timedelta(hours=random.randint(0, 12))
